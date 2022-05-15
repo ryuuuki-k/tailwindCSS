8be06b657ec1583fa053b7b0fca6f7937ffb44fd
ryuki.k <ryuki.k0120@gmail.com>
2022-05-16 06:36:23 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return <div className="py-3 bottom-3 m-2 mx-2 flex p-1"></div>;
+};
+
+export default Home;
